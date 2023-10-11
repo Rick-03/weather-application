1b4d01e893e8f3027126aa52178ae984339d8011
rick-03 <satwick.edu@gmail.com>
2023-10-11 23:28:30 +0530

    basic changes in javascript

--- script.js
+++ script.js
@@ -9,43 +9,51 @@ const weatheIcon=document.querySelector(".weather-icon");
 
 async function checkWeather(city){
     const response=await fetch(apiurl + city + `&appid=${apikey}`);
-    var data=await response.json();
 
-    console.log(data);
-    document.querySelector(".city").innerHTML=data.name;
-
-    document.querySelector(".temp").innerHTML=Math.round(data.main.temp) + "°c";
-
-    document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
-
-    document.querySelector(".wind").innerHTML=data.wind.speed+"km";
-
-
-    if(data.weather[0].main == "Clouds"){
-        weatheIcon.src ="images/clouds.png";
-
-    }else if(data.weather[0].main == "Clear"){
-        weatheIcon.src ="images/clear.png";
-    }
-    else if(data.weather[0].main == "Rain"){
-        weatheIcon.src ="images/rain.png";
-    }
-    else if(data.weather[0].main == "Drizzle"){
-        weatheIcon.src ="images/drizzle.png";
-    }
-    else if(data.weather[0].main == "Mist"){
-        weatheIcon.src ="images/mist.png";
-    }
-    else if(data.weather[0].main == "Haze"){
-        weatheIcon.src ="images/haze.png";
-    }
-    else if(data.weather[0].main == "Snow"){
-        weatheIcon.src ="images/snow.png";
+    if(response.status==404){
+        document.querySelector(".error").style.display="block";
+        document.querySelector(".weather").style.display="none";
     }
 
+    else{
+        var data=await response.json();
 
+        console.log(data);
+        document.querySelector(".city").innerHTML=data.name;
+    
+        document.querySelector(".temp").innerHTML=Math.round(data.main.temp) + "°c";
+    
+        document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
+    
+        document.querySelector(".wind").innerHTML=data.wind.speed+"km";
+    
+    
+        if(data.weather[0].main == "Clouds"){
+            weatheIcon.src ="images/clouds.png";
+    
+        }else if(data.weather[0].main == "Clear"){
+            weatheIcon.src ="images/clear.png";
+        }
+        else if(data.weather[0].main == "Rain"){
+            weatheIcon.src ="images/rain.png";
+        }
+        else if(data.weather[0].main == "Drizzle"){
+            weatheIcon.src ="images/drizzle.png";
+        }
+        else if(data.weather[0].main == "Mist"){
+            weatheIcon.src ="images/mist.png";
+        }
+        else if(data.weather[0].main == "Haze"){
+            weatheIcon.src ="images/haze.png";
+        }
+        else if(data.weather[0].main == "Snow"){
+            weatheIcon.src ="images/snow.png";
+        }
+        document.querySelector(".weather").style.display="block";
+        document.querySelector(".error").style.display="none";
+    }
+   
 }
-
 
 searchBtn.addEventListener("click",()=>{
     checkWeather(searchBox.value);
